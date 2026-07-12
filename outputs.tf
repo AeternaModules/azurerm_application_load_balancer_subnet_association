902914bb16cb629b5bbde77fc6406e73cf915d6c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "application_load_balancer_subnet_associations" {
-  description = "All application_load_balancer_subnet_association resources"
-  value       = azurerm_application_load_balancer_subnet_association.application_load_balancer_subnet_associations
-}
 output "application_load_balancer_subnet_associations_application_load_balancer_id" {
-  description = "List of application_load_balancer_id values across all application_load_balancer_subnet_associations"
-  value       = [for k, v in azurerm_application_load_balancer_subnet_association.application_load_balancer_subnet_associations : v.application_load_balancer_id]
+  description = "Map of application_load_balancer_id values across all application_load_balancer_subnet_associations, keyed the same as var.application_load_balancer_subnet_associations"
+  value       = { for k, v in azurerm_application_load_balancer_subnet_association.application_load_balancer_subnet_associations : k => v.application_load_balancer_id }
 }
 output "application_load_balancer_subnet_associations_name" {
-  description = "List of name values across all application_load_balancer_subnet_associations"
-  value       = [for k, v in azurerm_application_load_balancer_subnet_association.application_load_balancer_subnet_associations : v.name]
+  description = "Map of name values across all application_load_balancer_subnet_associations, keyed the same as var.application_load_balancer_subnet_associations"
+  value       = { for k, v in azurerm_application_load_balancer_subnet_association.application_load_balancer_subnet_associations : k => v.name }
 }
 output "application_load_balancer_subnet_associations_subnet_id" {
-  description = "List of subnet_id values across all application_load_balancer_subnet_associations"
-  value       = [for k, v in azurerm_application_load_balancer_subnet_association.application_load_balancer_subnet_associations : v.subnet_id]
+  description = "Map of subnet_id values across all application_load_balancer_subnet_associations, keyed the same as var.application_load_balancer_subnet_associations"
+  value       = { for k, v in azurerm_application_load_balancer_subnet_association.application_load_balancer_subnet_associations : k => v.subnet_id }
 }
 output "application_load_balancer_subnet_associations_tags" {
-  description = "List of tags values across all application_load_balancer_subnet_associations"
-  value       = [for k, v in azurerm_application_load_balancer_subnet_association.application_load_balancer_subnet_associations : v.tags]
+  description = "Map of tags values across all application_load_balancer_subnet_associations, keyed the same as var.application_load_balancer_subnet_associations"
+  value       = { for k, v in azurerm_application_load_balancer_subnet_association.application_load_balancer_subnet_associations : k => v.tags }
 }
 
